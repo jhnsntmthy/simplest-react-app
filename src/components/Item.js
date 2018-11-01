2432261841dbe422e1f6690c3f0117f24b1bf993
Cave Johnson <jhnsntmthy@gmail.com>
2018-11-01 09:01:44 -0400

    simple app

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ItemReactified extends React.Component {
+ //static propTypes = {};
+
+ render() {
+   return (
+     <div>
+       <h1>Item Reactified Component</h1>
+     </div>
+   )
+ }
+}
+
+export default ItemReactified;
